18d6db2d549690d7787f3e42994dd22f39d16612
Carlo <clatasa@gmail.com>
2026-03-27 10:05:50 -0400

    Initial Commit

---
+++ StorePalTests/StorePalTests.swift
@@ -0,0 +1,17 @@
+//
+//  StorePalTests.swift
+//  StorePalTests
+//
+//  Created by Carlo Latasa on 3/27/26.
+//
+
+import Testing
+@testable import StorePal
+
+struct StorePalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
